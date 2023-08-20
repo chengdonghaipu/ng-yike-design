f96975b8e6b91f4a9cedfb51a71e3e6769d71420
chendong <1296942311@qq.com>
2023-08-20 17:08:51 +0800

    build(project): 调整components的位置

--- components/public-api.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://github.com/chengdonghaipu/ng-yike-design/blob/master/LICENSE
- */
-
-export default void 0;
