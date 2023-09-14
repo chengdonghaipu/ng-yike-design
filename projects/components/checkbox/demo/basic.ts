a4176eb29a3c39f3ec479426cababc75fd54326b
chendong <1296942311@qq.com>
2023-09-15 03:57:31 +0800

    docs(checkbox): add checkbox docs

---
+++ projects/components/checkbox/demo/basic.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { NxCheckboxModule } from 'ng-yk-design/checkbox';
+import { NxSpaceComponent } from 'ng-yk-design/space';
+
+@Component({
+  selector: 'app-demo-checkbox-basic',
+  standalone: true,
+  imports: [CommonModule, ...NxCheckboxModule, NxSpaceComponent],
+  template: `
+    <nx-space>
+      <nx-checkbox [checked]="true">checked</nx-checkbox>
+      <nx-checkbox [indeterminate]="true">indeterminate</nx-checkbox>
+    </nx-space>
+  `,
+  styles: [``],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BasicComponent {}
