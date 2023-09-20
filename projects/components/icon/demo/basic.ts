73335ac3f77e5b307e1dbb69cd97b8bea6b19bb5
chendong <1296942311@qq.com>
2023-09-20 17:25:41 +0800

    docs(icon): add basic demo

--- projects/components/icon/demo/basic.ts
+++ projects/components/icon/demo/basic.ts
@@ -1,17 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { NxIconModule } from 'ng-yk-design/icon';
+import { NxButtonModule } from 'ng-yk-design/button';
+import { NxIconModule, PlusOutline, provideIcons, QuestionFill, QuestionOutline } from 'ng-yk-design/icon';
 
 @Component({
   selector: 'app-demo-icon-basic',
   standalone: true,
-  imports: [CommonModule, ...NxIconModule],
+  imports: [CommonModule, ...NxIconModule, ...NxButtonModule],
   template: `
-    <span nx-icon></span>
-    <nx-icon></nx-icon>
+    <span class="demo-icon" nx-icon icon="question" size="24px"></span>
+    <nx-icon class="demo-icon" icon="question" type="outline" size="24px"></nx-icon>
+    <nx-icon class="demo-icon" icon="app" type="fill" size="24px"></nx-icon>
+    <button nx-button nxShape="circle">
+      <nx-icon icon="plus" type="outline"></nx-icon>
+    </button>
   `,
-  styles: [``],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styles: [
+    `
+      .demo-icon {
+        margin-right: 6px;
+      }
+    `
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [provideIcons([QuestionFill, QuestionOutline, PlusOutline])]
 })
 export class BasicComponent {}
