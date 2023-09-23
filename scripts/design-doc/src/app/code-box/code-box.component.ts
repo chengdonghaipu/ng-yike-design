7187367c5dc7a486ef388a4780f30b81c7e33b4f
chendong <1296942311@qq.com>
2023-09-23 18:21:32 +0800

    docs(style): demo code 样式与vue版本对齐

--- scripts/design-doc/src/app/code-box/code-box.component.ts
+++ scripts/design-doc/src/app/code-box/code-box.component.ts
@@ -3,26 +3,39 @@ import {NgClass, NgIf} from '@angular/common';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {DomSanitizer} from "@angular/platform-browser";
+import {NxSpaceComponent} from "ng-yk-design/space";
+import {NxIconComponent} from "ng-yk-design/icon";
+import {NxButtonComponent} from "ng-yk-design/button";
 
 @Component({
   selector: 'app-code-box',
   standalone: true,
-  imports: [NgClass, NgIf],
+  imports: [NgClass, NgIf, NxSpaceComponent, NxIconComponent, NxButtonComponent],
   template: `
     <section class="code-box" [ngClass]="{ expand: nxExpanded }" [attr.id]="nxId">
+      <h3 class="code-box-title">{{ nxTitle }} <a href="#{{nxId}}">#</a></h3>
+      <div class="code-box-description">
+        <ng-content select="[intro]"></ng-content>
+      </div>
       <section class="code-box-demo">
         <div>
           <ng-content select="[demo]"></ng-content>
         </div>
       </section>
-      <section class="code-box-meta markdown">
-        <div class="code-box-title">
+      <section class="code-box-meta" nx-space block justify="end">
+        <button nx-button nxShape="square" nxType="outline">
+          <nx-icon icon="copy" type="outline" size="20px"></nx-icon>
+        </button>
+        <button nx-button nxShape="square" [class.select]="nxExpanded" (click)="expandCode(!nxExpanded)">
+          <nx-icon icon="code" type="outline" size="20px"></nx-icon>
+        </button>
+        <!--<div class="code-box-title">
           <span> {{ nxTitle }} </span>
         </div>
         <div class="code-box-description">
           <ng-content select="[intro]"></ng-content>
-        </div>
-        <div class="code-box-actions">
+        </div>-->
+        <!--<div class="code-box-actions">
           <span
             class="code-box-code-copy"
             [class.ant-tooltip-open]="copied"
@@ -42,7 +55,7 @@ import {DomSanitizer} from "@angular/platform-browser";
               [class.code-expand-icon-hide]="nxExpanded"
             />
           </span>
-        </div>
+        </div>-->
       </section>
       <section class="code-content" [innerHTML]="safeCode()" *ngIf="nxExpanded">
       </section>
@@ -58,20 +71,27 @@ import {DomSanitizer} from "@angular/platform-browser";
       transition: opacity .3s;
     }
     .code-box {
-      background-color: #fff;
-      position: relative;
-      display: inline-block;
-      width: 100%;
-      margin: 0 0 16px;
-      border: 1px solid rgba(0,0,0,.06);
-      border-radius: 2px;
-      transition: all .2s;
+      margin-top: 28px;
+      max-width: 1200px;
+      //background-color: #fff;
+      //position: relative;
+      //display: inline-block;
+      //width: 100%;
+      //margin: 0 0 16px;
+      //border: 1px solid rgba(0,0,0,.06);
+      //border-radius: 2px;
+      //transition: all .2s;
     }
     .code-box-demo {
-      padding: 42px 24px 50px;
-      color: #000000d9;
-      border-bottom: 1px solid rgba(0,0,0,.06);
-      background-color: #fff;
+      //padding: 42px 24px 50px;
+      //color: #000000d9;
+      //border-bottom: 1px solid rgba(0,0,0,.06);
+      //background-color: #fff;
+      margin: 12px 0 8px;
+      padding: 20px;
+      border: 1px solid rgba(var(--yk-color-info-rgb),.08);
+      border-radius: 8px;
+      transition: all .3s;
     }
     .code-box .code-expand-icon-show {
       opacity: .55;
@@ -108,26 +128,43 @@ import {DomSanitizer} from "@angular/platform-browser";
     }
 
     .code-box-description {
-      padding: 18px 24px 12px;
+      //padding: 18px 24px 12px;
     }
 
     .code-box-title {
-      position: absolute;
-      top: -14px;
-      margin-left: 16px;
-      padding: 1px 8px;
-      color: #000000d9;
-      background: #fff;
-      border-radius: 2px 2px 0 0;
-      transition: background-color .4s;
+      font-size: 24px;
+      line-height: 1.34;
+      margin-top: 1em;
+      margin-bottom: 0.5em;
+      font-weight: 600;
+      > a {
+        opacity: 0;
+        //transition: none;
+        transition: opacity .4s;
+        &:hover {
+          opacity: 1;
+        }
+      }
+      //position: absolute;
+      //top: -14px;
+      //margin-left: 16px;
+      //padding: 1px 8px;
+      //color: #000000d9;
+      //background: #fff;
+      //border-radius: 2px 2px 0 0;
+      //transition: background-color .4s;
     }
 
-    .code-box-meta.markdown {
-      position: relative;
-      width: 100%;
-      font-size: 14px;
-      border-radius: 0 0 2px 2px;
-      transition: background-color .4s;
+    .code-box-meta {
+      button.select {
+        background-color: rgb(var(--yk-color-info-rgb));
+        color: var(--yk-color-white);
+      }
+      //position: relative;
+      //width: 100%;
+      //font-size: 14px;
+      //border-radius: 0 0 2px 2px;
+      //transition: background-color .4s;
     }
   `]
 })
