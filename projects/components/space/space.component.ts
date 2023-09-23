dc8e9da628e95829c9847ff9fcc4d2a2c5023930
chendong <1296942311@qq.com>
2023-09-23 18:05:07 +0800

    feat(space): space支持block justify

--- projects/components/space/space.component.ts
+++ projects/components/space/space.component.ts
@@ -3,18 +3,7 @@
  * found in the LICENSE file at https://github.com/chengdonghaipu/ng-yike-design/blob/master/LICENSE
  */
 
-import { CommonModule } from '@angular/common';
-import {
-  booleanAttribute,
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  Directive,
-  effect,
-  Input,
-  signal,
-  ViewEncapsulation
-} from '@angular/core';
+import { booleanAttribute, computed, Directive, effect, Input, signal } from '@angular/core';
 
 import { onChanges, TypeObject, useHostDom } from 'ng-yk-design/core';
 import { getCssVar, watchInputs } from 'ng-yk-design/core/util';
@@ -23,12 +12,15 @@ type numberInput = number | string;
 type NxSize = 'small' | 'medium' | 'large' | 'xLarge' | numberInput | [numberInput, numberInput];
 type AlignItems = 'start' | 'end' | 'center' | 'baseline' | 'stretch';
 type LayoutDirection = 'vertical' | 'horizontal';
+type JustifyContent = 'start' | 'end' | 'center' | 'between' | 'around' | 'evenly';
 
 interface NxSpaceInputs {
   nxSize: NxSize;
   wrap: string | boolean;
+  block: string | boolean;
   nxAlign: AlignItems | null;
   nxDirection: LayoutDirection | null;
+  justify: JustifyContent | null;
 }
 
 function withSpaceInputs<T extends NxSpaceInputs>(this: T): void {
@@ -38,6 +30,8 @@ function withSpaceInputs<T extends NxSpaceInputs>(this: T): void {
   const nxWrap = signal(this.wrap);
   const nxAlign = signal(this.nxAlign);
   const nxDirection = signal(this.nxDirection);
+  const nxBlock = signal(this.block);
+  const nxJustify = signal(this.justify);
 
   const computedSize = computed(() => {
     const size = nxSize();
@@ -56,14 +50,26 @@ function withSpaceInputs<T extends NxSpaceInputs>(this: T): void {
     const wrap = nxWrap();
     const align = nxAlign();
     const direction = nxDirection();
+    const block = nxBlock();
+    const justify = nxJustify();
 
     const directionMap: TypeObject<string> = { vertical: 'column', horizontal: 'row' };
+
+    const flexMap: TypeObject<string> = {
+      start: 'flex-start',
+      end: 'flex-end',
+      between: 'space-between',
+      around: 'space-around',
+      evenly: 'space-evenly'
+    };
 
     const style: Partial<CSSStyleDeclaration> = {
       gap: size,
       flexWrap: wrap ? 'wrap' : 'nowrap',
       ...(align && { alignItems: align }),
-      ...(direction && { flexDirection: directionMap[direction] })
+      ...(direction && { flexDirection: directionMap[direction] }),
+      ...(block && { display: 'flex' }),
+      ...(justify && { justifyContent: flexMap[justify] ?? justify })
     };
 
     return style;
@@ -71,11 +77,13 @@ function withSpaceInputs<T extends NxSpaceInputs>(this: T): void {
 
   onChanges.call(
     this,
-    watchInputs(['nxSize', 'wrap', 'nxAlign', 'nxDirection'], (key, change) => {
+    watchInputs(['nxSize', 'wrap', 'nxAlign', 'nxDirection', 'block', 'justify'], (key, change) => {
       key === 'nxSize' && nxSize.set(change.currentValue);
       key === 'wrap' && nxWrap.set(change.currentValue);
       key === 'nxAlign' && nxAlign.set(change.currentValue);
       key === 'nxDirection' && nxDirection.set(change.currentValue);
+      key === 'block' && nxBlock.set(change.currentValue);
+      key === 'justify' && nxJustify.set(change.currentValue);
     })
   );
 
@@ -92,8 +100,10 @@ function withSpaceInputs<T extends NxSpaceInputs>(this: T): void {
 export class NxSpaceComponent implements NxSpaceInputs {
   @Input() nxSize: NxSize = 'large';
   @Input({ transform: booleanAttribute }) wrap = false;
+  @Input({ transform: booleanAttribute }) block = false;
   @Input() nxAlign: AlignItems | null = null;
   @Input() nxDirection: LayoutDirection | null = null;
+  @Input() justify: JustifyContent | null = null;
 
   constructor() {
     withSpaceInputs.call(this);
