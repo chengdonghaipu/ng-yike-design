8171d7a16a3cf175fc943c39abca87917f5fa462
chendong <1296942311@qq.com>
2023-09-24 12:32:02 +0800

    docs(typography): 编写title使用文档

--- projects/components/typography/demo/title.md
+++ projects/components/typography/demo/title.md
@@ -7,7 +7,14 @@ title:
 
 ## zh-CN
 
-不同级别的标题
+如果通过`<nx-title></nx-title>`直接声明组件，可以使用`level`输入属性来设置标题级别
+```html
+<nx-title level="1">H1 欢迎使用 Yike Design</nx-title>
+```
+如果以属性的形式声明组件, 不能再使用`level`输入属性来设置标题级别，如下示例是错误:
+```html
+<h1 nx-title level="1">H1 欢迎使用 Yike Design</h1>
+```
 
 ## en-US
 
--- projects/components/typography/demo/title.ts
+++ projects/components/typography/demo/title.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { NxTypographyModule } from 'ng-yk-design/typography';
+
 @Component({
   selector: 'app-demo-typography-title',
   standalone: true,
-  imports: [CommonModule],
-  template: ``,
+  imports: [CommonModule, NxTypographyModule],
+  template: `
+    <h1 nx-title>H1 欢迎使用 Yike Design</h1>
+    <h2 nx-title>H2 欢迎使用 Yike Design</h2>
+    <h3 nx-title>H3 欢迎使用 Yike Design</h3>
+    <h4 nx-title>H4 欢迎使用 Yike Design</h4>
+    <h5 nx-title>H5 欢迎使用 Yike Design</h5>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TitleComponent {}
